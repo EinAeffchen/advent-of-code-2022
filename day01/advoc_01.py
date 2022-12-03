82db91aab9e99caac16388b2e9fc7942c905d96b
einaeffchen(猿） <github@dummer.dev>
2022-12-03 13:13:53 +0900

    Fix typing

--- day01/advoc_01.py
+++ day01/advoc_01.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 from typing import Generator
 
-def read_input() -> Generator[int, None, None]:
+def read_input() -> map:
     with open(Path(__file__).parent/"input", "r") as i:
         return map(lambda x: sum(map(int, x.strip().split("\n"))), i.read().split("\n\n"))
          
-def get_highest_calories(data: Generator[int, None, None]):
+def get_highest_calories(data: map):
     return max(data)
 
-def get_sum_calories_for_top_n(data: Generator[int, None, None], n: int):
+def get_sum_calories_for_top_n(data: map, n: int):
     return sum(sorted(data)[-n:])
 
 if __name__ == '__main__':
