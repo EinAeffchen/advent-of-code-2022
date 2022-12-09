c8f513f341090eeaf72f8e5fa53d64d7de2fc445
einaeffchen <leon.dummer@bundesanzeiger.de>
2022-12-09 22:22:52 +0900

    Add day09

--- day09/advoc_09.py
+++ day09/advoc_09.py
@@ -7,50 +7,50 @@ def read_input() -> map:
         return list(map(lambda x: x.split(), i.readlines()))
 
 
-def calc_t(movement: list[str, int], rope: list, i: int):
-    direction = movement[0]
-    distance = int(movement[1])
+def follow(h_pos: list, t_pos: list, direction: str):
+    if direction == "U" and abs(t_pos[1] - h_pos[1]) > 1:
+        t_pos[1] += 1
+        if diff := h_pos[0] - t_pos[0]:
+            t_pos[0] += diff
+    elif direction == "D" and abs(t_pos[1] - h_pos[1]) > 1:
+        t_pos[1] -= 1
+        if diff := h_pos[0] - t_pos[0]:
+            t_pos[0] += diff
+    elif direction == "R" and abs(t_pos[0] - h_pos[0]) > 1:
+        t_pos[0] += 1
+        if diff := h_pos[1] - t_pos[1]:
+            t_pos[1] += diff
+    elif direction == "L" and abs(t_pos[0] - h_pos[0]) > 1:
+        t_pos[0] -= 1
+        if diff := h_pos[1] - t_pos[1]:
+            t_pos[1] += diff
 
-    h_pos = rope[i]
-    t_pos = rope[i + 1]
 
-    for _ in range(distance):
-        if direction == "U":
-            h_pos[1] += 1
-            if abs(t_pos[1] - h_pos[1]) > 1:
-                t_pos[1] += 1
-                if diff := h_pos[0] - t_pos[0]:
-                    t_pos[0] += diff
-        elif direction == "D":
-            h_pos[1] -= 1
-            if abs(t_pos[1] - h_pos[1]) > 1:
-                t_pos[1] -= 1
-                if diff := h_pos[0] - t_pos[0]:
-                    t_pos[0] += diff
-        elif direction == "R":
-            h_pos[0] += 1
-            if abs(t_pos[0] - h_pos[0]) > 1:
-                t_pos[0] += 1
-                if diff := h_pos[1] - t_pos[1]:
-                    t_pos[1] += diff
-        elif direction == "L":
-            h_pos[0] -= 1
-            if abs(t_pos[0] - h_pos[0]) > 1:
-                t_pos[0] -= 1
-                if diff := h_pos[1] - t_pos[1]:
-                    t_pos[1] += diff
-        # print(t_pos)
-        if i == 8:
-            t_positions.add(tuple(t_pos))
+def calc_t(direction: str, h_pos: list):
+    if direction == "U":
+        h_pos[1] += 1
+    elif direction == "D":
+        h_pos[1] -= 1
+    elif direction == "R":
+        h_pos[0] += 1
+    elif direction == "L":
+        h_pos[0] -= 1
 
 
 t_positions = set()
 
 if __name__ == "__main__":
     data = read_input()
-    rope = [[0, 0] for _ in range(10)]
+    rope_length = 10  # part 2
+    rope = [[0, 0] for _ in range(rope_length)]
     # part 2 still broken
     for movement in data:
-        for i, knot in enumerate(rope[:-1]):
-            calc_t(movement, rope, i)
+        direction = movement[0]
+        distance = int(movement[1])
+        for _ in range(distance):
+            calc_t(direction, rope[0])
+            for i in range(rope_length - 1):
+                follow(rope[i], rope[i + 1], direction)
+                if i == rope_length - 2:
+                    t_positions.add(tuple(rope[i + 1]))
     print(len(t_positions))
